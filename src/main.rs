1754423e96921235ce1624cc11272ee7d62c49ee
achuie <andrew@ascent.ai>
2021-02-08 00:06:35 +0900

    Start model of cube.

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,39 @@
 fn main() {}
+
+struct Cube {
+    cubies: Vec<i32>,
+}
+
+impl Cube {
+    fn new() -> Self {
+        Self {
+            cubies: {
+                let mut res = Vec::new();
+                for i in 0..20 {
+                    res.push(i);
+                }
+
+                res
+            },
+        }
+    }
+
+    fn mv(&mut self, turn: Turns) {
+        match turn {}
+    }
+}
+
+enum Turn {
+    U,
+    Up,
+    D,
+    Dp,
+    R,
+    Rp,
+    L,
+    Lp,
+    F,
+    Fp,
+    B,
+    Bp,
+}
