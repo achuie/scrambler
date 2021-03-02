b958d8221bfd459f20d7f2a8cc71ec2388ce3490
achuie <andrew@ascent.ai>
2021-03-03 00:37:55 +0900

    Add clockwise rotation.

--- src/main.rs
+++ src/main.rs
@@ -41,9 +41,13 @@ impl Cube {
                             res[4] = Face {
                                 tiles: {
                                     let mut ts = [[Color::White; 3]; 3];
-                                    for j in 0..3 {
-                                        for i in (0..3).rev() {
-                                            ts[j][2 - i] = self.faces[4].tiles[i][j];
+                                    // prime
+                                    // 00 01 02  02 12 22
+                                    // 10 11 12  01 11 21
+                                    // 20 21 22  00 10 20
+                                    for j in (0..3).rev() {
+                                        for i in 0..3 {
+                                            ts[2 - j][i] = self.faces[4].tiles[i][j];
                                         }
                                     }
 
@@ -55,7 +59,32 @@ impl Cube {
                         },
                     }
                 } else {
-                    asdf
+                    res[0] = Face {
+                        tiles: {
+                            let mut ts = self.faces[0].tiles;
+                            for i in 0..3 {
+                                ts[i] = self.faces[1].tiles[i];
+                            }
+
+                            ts
+                        },
+                    };
+                    res[4] = Face {
+                        tiles: {
+                            let mut ts = self.faces[0].tiles;
+                            // not prime
+                            // 00 01 02  20 10 00
+                            // 10 11 12  21 11 01
+                            // 20 21 22  22 12 02
+                            for j in 0..3 {
+                                for i in (0..3).rev() {
+                                    ts[j][2 - i] = self.faces[4].tiles[i][j];
+                                }
+                            }
+
+                            ts
+                        },
+                    }
                 }
             },
         }
