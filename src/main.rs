b7f64bb6f34a6a42548a3786fba5815d7e8cc4d2
achuie <andrew@ascent.ai>
2021-03-03 01:04:38 +0900

    Refactor into `rotate()` function.

--- src/main.rs
+++ src/main.rs
@@ -23,69 +23,23 @@ impl Cube {
 
     fn mv(&self, turn: Turn) -> Self {
         match turn {
-            Turn::U(prime, double) => {
-                if prime {
-                    Cube {
-                        faces: {
-                            let mut res = [Face; 6];
-                            res[0] = Face {
-                                tiles: {
-                                    let mut ts = self.faces[0].tiles;
-                                    for i in 0..3 {
-                                        ts[i] = self.faces[3].tiles[i];
-                                    }
-
-                                    ts
-                                },
-                            };
-                            res[4] = Face {
-                                tiles: {
-                                    let mut ts = [[Color::White; 3]; 3];
-                                    // prime
-                                    // 00 01 02  02 12 22
-                                    // 10 11 12  01 11 21
-                                    // 20 21 22  00 10 20
-                                    for j in (0..3).rev() {
-                                        for i in 0..3 {
-                                            ts[2 - j][i] = self.faces[4].tiles[i][j];
-                                        }
-                                    }
-
-                                    ts
-                                },
-                            };
-
-                            res
-                        },
-                    }
-                } else {
+            Turn::U(prime, double) => Cube {
+                faces: {
+                    let mut res = [Face; 6];
                     res[0] = Face {
                         tiles: {
                             let mut ts = self.faces[0].tiles;
                             for i in 0..3 {
-                                ts[i] = self.faces[1].tiles[i];
+                                ts[i] = self.faces[3].tiles[i];
                             }
 
                             ts
                         },
                     };
-                    res[4] = Face {
-                        tiles: {
-                            let mut ts = self.faces[0].tiles;
-                            // not prime
-                            // 00 01 02  20 10 00
-                            // 10 11 12  21 11 01
-                            // 20 21 22  22 12 02
-                            for j in 0..3 {
-                                for i in (0..3).rev() {
-                                    ts[j][2 - i] = self.faces[4].tiles[i][j];
-                                }
-                            }
+                    res[4] = self.faces[4].rotate(prime, double);
 
-                            ts
-                        },
-                    }
-                }
+                    res
+                },
             },
         }
     }
@@ -98,6 +52,46 @@ struct Face {
 impl Face {
     fn new(color: Color) -> Self {
         Face { tiles: [[color; 3]; 3] }
+    }
+
+    fn rotate(&self, prime: bool, double: bool) -> Face {
+        Face {
+            tiles: {
+                let mut tile_array = [[self.tiles[1][1]; 3]; 3];
+                if double {
+                    // 00 01 02    22 21 20
+                    // 10 11 12 -> 12 11 10
+                    // 20 21 22    02 01 00
+                    for i in (0..3).rev() {
+                        for j in (0..3).rev() {
+                            tile_array[2 - i][2 - j] = self.tiles[i][j];
+                        }
+                    }
+                } else {
+                    if prime {
+                        // 00 01 02    02 12 22
+                        // 10 11 12 -> 01 11 21
+                        // 20 21 22    00 10 20
+                        for j in (0..3).rev() {
+                            for i in 0..3 {
+                                tile_array[2 - j][i] = self.tiles[i][j];
+                            }
+                        }
+                    } else {
+                        // 00 01 02    20 10 00
+                        // 10 11 12 -> 21 11 01
+                        // 20 21 22    22 12 02
+                        for j in 0..3 {
+                            for i in (0..3).rev() {
+                                tile_array[j][2 - i] = self.tiles[i][j];
+                            }
+                        }
+                    }
+                }
+
+                tile_array
+            },
+        }
     }
 }
 
