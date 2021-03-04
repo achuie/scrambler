b6a133e5c05a2c2fe78933b452f013194ef7db26
achuie <andrew@ascent.ai>
2021-03-04 10:36:53 +0900

    Flesh out more turn algorithms before generalization.

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,170 @@
 fn main() {}
 
 struct Cube {
-    faces: [Face; 6],
+    green: Face,
+    red: Face,
+    blue: Face,
+    orange: Face,
+    white: Face,
+    yellow: Face,
 }
 
 impl Cube {
     fn new() -> Self {
         Cube {
-            faces: {
-                let mut res = [Face; 6];
-                res[0] = Face::new(Color::Green);
-                res[1] = Face::new(Color::Red);
-                res[2] = Face::new(Color::Blue);
-                res[3] = Face::new(Color::Orange);
-                res[4] = Face::new(Color::White);
-                res[5] = Face::new(Color::Yellow);
-
-                res
-            },
+            green: Face::new(Color::Green),
+            red: Face::new(Color::Red),
+            blue: Face::new(Color::Blue),
+            orange: Face::new(Color::Orange),
+            white: Face::new(Color::White),
+            yellow: Face::new(Color::Yellow),
         }
     }
 
     fn mv(&self, turn: Turn) -> Self {
         match turn {
-            Turn::U(prime, double) => Cube {
-                faces: {
-                    let mut res = [Face; 6];
-                    res[0] = Face {
-                        tiles: {
-                            let mut ts = self.faces[0].tiles;
-                            for i in 0..3 {
-                                ts[i] = self.faces[3].tiles[i];
+            Turn::U { prime, double } => {
+                let to_update = [self.green, self.red, self.blue, self.orange];
+                let updated = to_update
+                    .iter()
+                    .enumerate()
+                    .map(|(i, face)| {
+                        face.update_triplet(
+                            Triplet::Top,
+                            if double {
+                                to_update[(i + 2) % 4].tiles[0]
+                            } else {
+                                if prime {
+                                    to_update[(i - 1) % 4].tiles[0]
+                                } else {
+                                    to_update[(i + 1) % 4].tiles[0]
+                                }
+                            },
+                        )
+                    })
+                    .collect();
+
+                Cube {
+                    green: updated[0],
+                    red: updated[1],
+                    blue: updated[2],
+                    orange: updated[3],
+                    white: self.white.rotate(prime, double),
+                    yellow: self.yellow.clone(),
+                }
+            },
+            Turn::D { prime, double } => {
+                let to_update = [self.green, self.red, self.blue, self.orange];
+                let updated = to_update
+                    .iter()
+                    .enumerate()
+                    .map(|(i, face)| {
+                        face.update_triplet(
+                            Triplet::Bottom,
+                            if double {
+                                to_update[(i + 2) % 4].tiles[2]
+                            } else {
+                                if prime {
+                                    to_update[(i - 1) % 4].tiles[2]
+                                } else {
+                                    to_update[(i + 1) % 4].tiles[2]
+                                }
+                            },
+                        )
+                    })
+                    .collect();
+
+                Cube {
+                    green: updated[0],
+                    red: updated[1],
+                    blue: updated[2],
+                    orange: updated[3],
+                    white: self.white.clone(),
+                    yellow: self.yellow.rotate(prime, double),
+                }
+            },
+            Turn::R { prime, double } => {
+                let to_update = [self.green, self.yellow, self.blue, self.white];
+                let update_sections =
+                    [Triplet::Right, Triplet::Right, Triplet::Left, Triplet::Right];
+                let updated = to_update
+                    .iter()
+                    .enumerate()
+                    .map(|(i, face)| {
+                        let other_idx = {
+                            if double {
+                                (i + 2) % 4
+                            } else {
+                                if prime {
+                                    (i - 1) % 4
+                                } else {
+                                    (i + 1) % 4
+                                }
                             }
+                        };
 
-                            ts
-                        },
-                    };
-                    res[4] = self.faces[4].rotate(prime, double);
+                        face.update_triplet(
+                            update_sections[i],
+                            to_update[other_idx].get_triplet(update_sections[other_idx]),
+                        )
+                    })
+                    .collect();
 
-                    res
-                },
+                Cube {
+                    green: updated[0],
+                    red: self.red.rotate(prime, double),
+                    blue: updated[2],
+                    orange: self.orange.clone(),
+                    white: updated[3],
+                    yellow: updated[1],
+                }
             },
         }
     }
 }
 
+#[derive(Copy)]
 struct Face {
-    pub tiles: [[Color; 3]; 3],
+    tiles: [[Color; 3]; 3],
 }
 
 impl Face {
     fn new(color: Color) -> Self {
         Face { tiles: [[color; 3]; 3] }
+    }
+
+    fn get_triplet(&self, section: Triplet) -> [Color; 3] {
+        match section {
+            Triplet::Top => self.tiles[0],
+            Triplet::Right => self.tiles.iter().map(|row| row[2]).collect(),
+            Triplet::Bottom => self.tiles[2],
+            Triplet::Left => self.tiles.iter().map(|row| row[0]).collect(),
+        }
+    }
+
+    fn update_triplet(&self, section: Triplet, cubies: [Color; 3]) -> Face {
+        Face {
+            tiles: {
+                match section {
+                    Triplet::Top => [cubies, self.tiles[1], self.tiles[2]],
+                    Triplet::Right => {
+                        let mut tile_array = self.tiles.clone();
+                        for i in 0..3 {
+                            tile_array[i][2] = cubies[i];
+                        }
+
+                        tile_array
+                    },
+                    Triplet::Bottom => [self.tiles[0], self.tiles[1], cubies],
+                    Triplet::Left => {
+                        let mut tile_array = self.tiles.clone();
+                        for i in 0..3 {
+                            tile_array[i][0] = cubies[i];
+                        }
+                    },
+                }
+            },
+        }
     }
 
     fn rotate(&self, prime: bool, double: bool) -> Face {
@@ -93,6 +206,13 @@ impl Face {
             },
         }
     }
+}
+
+enum Triplet {
+    Top,
+    Right,
+    Bottom,
+    Left,
 }
 
 enum Color {
