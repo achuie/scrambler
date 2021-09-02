9ebd9ddfad8c537ba098b7909beaaca2728cd27b
Andrew Huie <achuie@protonmail.com>
2021-09-03 00:52:12 +0900

    Add shell.nix to generate lockfile with nix-provided cargo.

---
+++ shell.nix
@@ -0,0 +1,15 @@
+let
+  mozilla-pkgs = import (builtins.fetchTarball
+    https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  pkgs = import <nixpkgs> {
+    overlays = [
+      mozilla-pkgs (self: super:
+      {
+        rustc = (self.rustChannelOf { rustToolchain = ./rust-toolchain; }).rust;
+        cargo = (self.rustChannelOf { rustToolchain = ./rust-toolchain; }).rust;
+      })
+    ];
+  };
+in pkgs.mkShell {
+  inputsFrom = with pkgs; [ rustc cargo ];
+}
