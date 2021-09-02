2b1ff27785d6a711291cf817e9e84945f2c6d6b4
achuie <31836492+achuie@users.noreply.github.com>
2021-09-02 11:40:12 +0900

    Add nix build file.

---
+++ default.nix
@@ -0,0 +1,19 @@
+let
+  mozilla-pkgs = import (builtins.fetchTarball
+    https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  pkgs = import <nixpkgs> {
+    overlays = [
+      mozilla-pkgs (self: super:
+      {
+        rustc = (self.rustChannelOf { rustToolchain = ./rust-toolchain; }).rust;
+        cargo = (self.rustChannelOf { rustToolchain = ./rust-toolchain; }).rust;
+      })
+    ];
+  };
+  naersk = pkgs.callPackage (builtins.fetchGit {
+    name = "HEAD";
+    url = "https://github.com/nix-community/naersk";
+    ref = "HEAD";
+    rev = "df71f5e4babda41cd919a8684b72218e2e809fa9";
+  }) {};
+in naersk.buildPackage ./.
