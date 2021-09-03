f2e8642811d9c32f9ae22ae0976f99952b267a47
Andrew Huie <achuie@protonmail.com>
2021-09-03 23:53:37 +0900

    Refactor into shared toolchain definition.

--- default.nix
+++ default.nix
@@ -1,15 +1,5 @@
 let
-  mozilla-pkgs = import (builtins.fetchTarball
-    https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
-  pkgs = import <nixpkgs> {
-    overlays = [
-      mozilla-pkgs (self: super:
-      {
-        rustc = (self.rustChannelOf { rustToolchain = ./rust-toolchain; }).rust;
-        cargo = (self.rustChannelOf { rustToolchain = ./rust-toolchain; }).rust;
-      })
-    ];
-  };
+  pkgs = import ./rust_toolchain.nix;
   naersk = pkgs.callPackage (builtins.fetchGit {
     name = "master";
     url = "https://github.com/nix-community/naersk";
--- shell.nix
+++ shell.nix
@@ -1,15 +1,5 @@
-let
-  mozilla-pkgs = import (builtins.fetchTarball
-    https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
-  pkgs = import <nixpkgs> {
-    overlays = [
-      mozilla-pkgs (self: super:
-      {
-        rustc = (self.rustChannelOf { rustToolchain = ./rust-toolchain; }).rust;
-        cargo = (self.rustChannelOf { rustToolchain = ./rust-toolchain; }).rust;
-      })
-    ];
-  };
-in pkgs.mkShell {
+{ pkgs ? import ./rust_toolchain.nix }:
+
+pkgs.mkShell {
   inputsFrom = with pkgs; [ rustc cargo ];
 }
