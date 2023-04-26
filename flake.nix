3c0fd19e2bb0dfc3c89249f92981a4afe52e4828
achuie <achuie@protonmail.com>
2023-04-26 22:39:24 +0900

    Update flake.

--- flake.nix
+++ flake.nix
@@ -1,31 +1,29 @@
 {
   description = "Rubik's Cube scrambler";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs, utils, fenix, flake-compat }:
-    utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
-      {
-        defaultPackage = (pkgs.makeRustPlatform {
-          inherit (fenix.packages.${system}.minimal) rustc cargo;
-        }).buildRustPackage {
-          pname = "scrambler";
-          version = "0.1.0-alpha";
-          src = ./.;
-          cargoSha256 = "sha256-L70oVwjRil95xWrDa0ZxdqSyw9XAa5WSqs++nWT9eBo=";
-        };
-      }
-    );
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+          src = pkgs.lib.sourceByRegex self [ "src(/.*)?" "Cargo\\.(toml|lock)" ];
+        in
+        {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "scrambler";
+            inherit ((pkgs.lib.importTOML "${src}/Cargo.toml").package) version;
+
+            inherit src;
+            cargoLock = { lockFile = "${src}/Cargo.lock"; };
+          };
+        });
+      formatter = forAllSystems (system: nixpkgsFor.${system}.nixpkgs-fmt);
+    };
 }
